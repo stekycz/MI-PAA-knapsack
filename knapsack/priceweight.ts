75480cd155e1d52f93ff4f05cf17439ff95b5cc9
Martin Štekl <martin.stekl@gmail.com>
2013-10-07 12:11:16 +0200

    Updated implementation of priceweight heuristics

--- knapsack/priceweight.ts
+++ knapsack/priceweight.ts
@@ -5,50 +5,17 @@ import k = require("knapsack");
 class PriceWeight extends k.ProblemSolver {
 	public _find(items : k.Item[], maxWeight : number) : k.Knapsack {
 		items.sort(function (a : k.Item, b : k.Item) {
-			return (a.getPrice() / a.getWeight()) - (b.getPrice() / b.getWeight());
+			return (b.getPrice() / b.getWeight()) - (a.getPrice() / a.getWeight());
 		});
-		var combinations = PriceWeight._countCombinations(items.length);
 		var solution = new k.Knapsack();
-		var knapsack = new k.Knapsack();
 
-		for (var i = 0; i < combinations; i++) {
-			var j = items.length - 1;
-			while (knapsack.contains(items[j]) && j > 0) {
-				knapsack.removeItem(items[j]);
-				j--;
-			}
-			knapsack.addItem(items[j]);
-
-			if (PriceWeight._isBetterSolution(knapsack, solution, maxWeight)) {
-				solution.setItems(knapsack.getItems());
+		for (var i = 0; i < items.length; i++) {
+			if (solution.getWeight() + items[i].getWeight() <= maxWeight) {
+				solution.addItem(items[i]);
 			}
 		}
 
 		return solution;
-	}
-
-	private static _countCombinations(itemsCount : number) : number {
-		var combinations = 0;
-		if (itemsCount > 0) {
-			combinations = 2;
-		}
-		for (var i = 1; i < itemsCount; i++) {
-			combinations *= 2;
-		}
-		return combinations;
-	}
-
-	private static _isBetterSolution(knapsack : k.Knapsack, solution : k.Knapsack, maxWeight : number) : boolean {
-		return knapsack.getWeight() <= maxWeight && (
-			knapsack.getPrice() > solution.getPrice()
-			|| (
-				knapsack.getPrice() == solution.getPrice()
-				&& (
-				    knapsack.getItems().length > solution.getItems().length
-				    || knapsack.getWeight() > solution.getWeight()
-				)
-			)
-		);
 	}
 }
 
